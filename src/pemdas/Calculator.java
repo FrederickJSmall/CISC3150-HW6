b4be5e51965e0bd6be5423f0e7a80bc7e5c3f996
Fred Small <Fsmall@startoffsmall.com>
2015-11-08 11:22:30 -0500

    Validation logic for parenthesis

--- src/pemdas/Calculator.java
+++ src/pemdas/Calculator.java
@@ -12,6 +12,22 @@ public class Calculator {
 	{
 		return value1 + value2;
 	}
+	// Check to see if the parenthesis for the statemant is properly
+	// balanced 
+	boolean isParenthesisBalanced(String value)
+	{
+		int balanced = 0;
+		for (int i=0; i<value.length(); i++)
+		{
+			if (value.charAt(i)== '(')
+				balanced++;
+			if (value.charAt(i)== ')')
+				balanced--;
+	}
+		return balanced == 0 ? true : false; 
+	}
+	
+	
 	// Handle exponential power and root functions
 	double power (double value1, double value2)
 	{
